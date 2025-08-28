77720f7e7f448b3ced63c356e5a52226dec76e4e
ptiapa <pjtiapa2810@gmail.com>
2025-08-28 17:29:46 -0400

    Fix template y alias de URL para editar código

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -3,6 +3,7 @@ from .views import lista_proyectos, lista_flujo_caja, editar_flujo, importar_pro
 
 urlpatterns = [
     path('', lista_proyectos, name='lista_proyectos'),
+    path('', lista_proyectos, name='proyectos_lista'),
     path('flujo/', lista_flujo_caja, name='lista_flujo_caja'),
     path("flujo/<int:pk>/", editar_flujo, name="editar_flujo"),  
     path("importar/", importar_proyectos, name="proyectos_importar"),
